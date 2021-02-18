e884e41b463850c061768c71b165223a9e625f0f
Eebzie <gabrielle.agustin@gmail.com>
2021-02-18 10:30:06 -0500

    Last updated: 2/16/2021 -> to be modified 2/18/2021

---
+++ app.js
@@ -0,0 +1,10 @@
+const navSlide = () => {
+    const profileSettings = document.querySelector('.profileSettings');
+    const nav = document.querySelector('.nav-links');
+
+    profileSettings.addEventListener('click', () =>{
+        nav.classList.toggle('nav-active');
+        console.log("yes");
+    }
+}
+navSlide();
