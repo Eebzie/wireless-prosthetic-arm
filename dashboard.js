ca42a59dfe7eabb94018c740cf26cec18831e6e5
Eebzie <gabrielle.agustin@gmail.com>
2021-04-01 05:03:03 -0400

    Responsive content and consistent branding

--- dashboard.js
+++ dashboard.js
@@ -2,6 +2,7 @@ window.addEventListener("load", () => {
     document.body.classList.remove("preload");
 });
 
+// Nav Bar Open/Close
 document.addEventListener("DOMContentLoaded", () => {
 const nav = document.querySelector(".nav");
 
@@ -14,6 +15,13 @@ const nav = document.querySelector(".nav");
     });
 });
 
+// Chart Resizing for Window Size Change
+const myCanvas = document.getElementById("canvas"); 
+const updateCanvas = () => ({width: window.innerWidth, height: window.innerHeight}) 
+updateCanvas();
+window.addEventListener("resize", updateCanvas());
+
+// Dark Mode Toggle
 function dark_mode_toggle() {
     document.body.classList.toggle("light");
     document.body.classList.toggle("dark");
